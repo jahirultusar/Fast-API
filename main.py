c471ae243f9e6632d6d1183b2b67da6bb5bcbcd4
Jahirul Tusar <tusar86@gmail.com>
2026-01-18 21:02:50 +0000

    single post route bug fix and html url for update

--- main.py
+++ main.py
@@ -24,16 +24,32 @@ posts: list[dict] = [
     },
 ]
 
+# Home page route
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", context={"posts": posts, "title": "Home"},
-                                      )   
+    return templates.TemplateResponse(
+        request, "home.html", context={"posts": posts, "title": "Home"},
+    )   
 
+# Single blog post page route
+@app.get("/posts/{post_id}", include_in_schema=False)
+def single_post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(
+                request, "post.html", context={"post": post, "title": title},
+            )   
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Post not found!")
+
+# API route to get all posts
 @app.get("/api/posts")
 def get_posts():
     return posts
 
+# API route to get a single post by ID
 @app.get("/api/posts/{post_id}")
 def get_posts(post_id: int):
     for post in posts:
