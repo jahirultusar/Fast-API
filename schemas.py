318c0be22d4ad1e6accc1c689ddbd5e691526e22
Jahirul Tusar <tusar86@gmail.com>
2026-01-19 15:31:13 +0000

    schemas.py updated with relations to models.py

--- schemas.py
+++ schemas.py
@@ -1,19 +1,38 @@
-from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+
+## Base schema for user creation and response
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
+
 
 # Base schema for posts creation 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
 
 # Schema for creating a new post
 class PostCreate(PostBase):
-    pass
+    User_id: int    ##### TEMPORARY
 
 # Schema for responding with a post
 class PostResponse(PostBase):
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
 
-    model_config = ConfigDict(from_attributes=True)
+
 
